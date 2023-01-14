87a3b3650dfca4f426d70bef24dfa7706b2842ba
Kavita mali <kavita13feb@gmail.com>
2023-01-14 14:35:33 +0530

    nav

---
+++ kavita-mali-portfolio/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Flex, Spacer } from '@chakra-ui/react'
+import "./navbar.css"
+export const Navbar=()=>{
+return <div className='navbar'>
+ 
+<h3>Home</h3>
+<h3>About Me</h3>
+<h3>Skills</h3>
+<h3>Project</h3>
+<h3>Contect</h3>
+<h3>Reusme</h3>
+ 
+</div>
+}
